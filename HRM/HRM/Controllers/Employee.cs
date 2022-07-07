e44007199ca5db35b6480ac9072f68a58822ec92
Phap Nguyen <phaphtt@gmail.com>
2022-07-07 11:29:54 +0700

    push api Employee
    push api getInforEmployeeById

---
+++ HRM/HRM/Controllers/Employee.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace HRM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Employee : Controller
+    {
+        [HttpGet("getInfor/{id}")]
+        public IEnumerable<Models.InforDetailEmployee> GetInforEmployee(int id)
+        {
+            //Connect DB:
+            Models.MySQLsetting.ConnectToMySqlService();
+            string procedure = "GetInforEmployeeById";
+            MySqlCommand cmd = new MySqlCommand(procedure, Models.MySQLsetting.conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", id);
+            MySqlDataReader result = cmd.ExecuteReader();
+
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Models.InforDetailEmployee employee = new Models.InforDetailEmployee();
+                    employee.id = result.GetInt32("id");
+                    if (!result.IsDBNull(1))
+                        employee.idManager = result.GetInt32("idManager");
+                    if (!result.IsDBNull(2))
+                        employee.idDepartment = result.GetInt32("idDepartment");
+                    if (!result.IsDBNull(3))
+                        employee.idEmployee = result.GetString("idEmployee");
+                    if (!result.IsDBNull(4))
+                        employee.firstname = result.GetString("firstname");
+                    if (!result.IsDBNull(5))
+                        employee.lastname = result.GetString("lastname");
+                    if (!result.IsDBNull(6))
+                        employee.dayOfBirth = result.GetString("dayOfBirth");
+                    if (!result.IsDBNull(7))
+                        employee.gender = result.GetString("gender");
+                    if (!result.IsDBNull(8))
+                        employee.email = result.GetString("email");
+                    if (!result.IsDBNull(9))
+                        employee.phoneNumber = result.GetString("phoneNumber");
+                    if (!result.IsDBNull(10))
+                        employee.address = result.GetString("address");
+                    if (!result.IsDBNull(11))
+                        employee.maritalStatus = result.GetString("maritalStatus");
+                    if (!result.IsDBNull(12))
+                        employee.position = result.GetString("position");
+                    if (!result.IsDBNull(13))
+                        employee.active = result.GetInt32("active");
+                    if (!result.IsDBNull(14))
+                        employee.manager_idEmployee = result.GetString("manager_idEmployee");
+                    if (!result.IsDBNull(15))
+                        employee.manager_firstname = result.GetString("manager_firstname");
+                    if (!result.IsDBNull(16))
+                        employee.manager_lastname = result.GetString("manager_lastname");
+                    if (!result.IsDBNull(17))
+                        employee.manager_gender = result.GetString("manager_gender");
+                    if (!result.IsDBNull(18))
+                        employee.manager_idDepartment = result.GetInt32("manager_idDepartment");
+                    if (!result.IsDBNull(19))
+                        employee.manager_position = result.GetString("manager_position");
+                    if (!result.IsDBNull(20))
+                        employee.manager_email = result.GetString("manager_email");
+                    if (!result.IsDBNull(21))
+                        employee.manager_phoneNumber = result.GetString("manager_phoneNumber");
+                    yield return employee;
+                }
+            }
+        }
+    }
+}
---
+++ HRM/HRM/Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRM.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public int idManager { get; set; }
+        public int idDepartment { get; set; }
+        public string idEmployee { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string dayOfBirth { get; set; }
+        public string gender { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public string address { get; set; }
+        public string maritalStatus { get; set; }
+        public string position { get; set; }
+        public int active { get; set; }
+    }
+
+    public class InforDetailEmployee : Employee
+    {
+        public int manager_idDepartment { get; set; }
+        public string manager_idEmployee { get; set; }
+        public string manager_firstname { get; set; }
+        public string manager_lastname { get; set; }
+        public string manager_gender { get; set; }
+        public string manager_email { get; set; }
+        public string manager_phoneNumber { get; set; }
+        public string manager_position { get; set; }
+    }
+}
